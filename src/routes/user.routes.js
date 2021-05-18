f8ae04bb9226cb82c453411430880d071b88fd74
lchaconc <luis.chacon.campos@gmail.com>
2021-05-18 15:26:46 -0600

    Envio de correo para activación -Instalación de nodemaileer -Importación de módulo -Creación de funcionalidad e integración en contorlador

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,19 +1,32 @@
 const { Router } = require("express");
 const Usuario = require("../models/Usuario");
 const bcrypt = require('bcrypt');
+const mailer = require("../mailer");
 const saltRounds = 10;
 
 const router = Router();
 
-router.post("/", async (req, res) => {
+router.post("/crear-cuenta", async (req, res) => {
     const {correo, clave} = req.body;
   
   const hashed = await bcrypt.hash (clave, saltRounds )
   const nuevoUsuario = new Usuario({clave: hashed, correo} );
   console.log(nuevoUsuario);
-  const isOk = await nuevoUsuario.save();
-
-  res.json(isOk);
+  await nuevoUsuario.save();
+  const info = await mailer(correo, "abcdef456987"); 
+  res.json(info);
 });
+
+
+router.get("/activar-cuenta/:token", async (req, res) => {
+    console.log(req.params);
+    res.send(
+        `
+        <strong> Cuenta activada de forma satisfactoria ✌️ </strong>
+        <hr>
+        Puede iniciar sesión con su usuario y clave al siguiente link: 
+        `
+     )
+} )
 
 module.exports = router;
