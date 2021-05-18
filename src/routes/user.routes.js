4149331b16b654cb9ed12aaf012e9141e3afa1f2
lchaconc <luis.chacon.campos@gmail.com>
2021-05-18 14:51:37 -0600

    Encriptación de clave -Se instala y referencia bcrypt -se obtiene la clave mediante req.body -se parametriza en función hashs

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,15 @@
 const { Router } = require("express");
 const Usuario = require("../models/Usuario");
-
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const router = Router();
 
 router.post("/", async (req, res) => {
-  //console.log(req.body);
-  const nuevoUsuario = new Usuario(req.body);
+    const {correo, clave} = req.body;
+  
+  const hashed = await bcrypt.hash (clave, saltRounds )
+  const nuevoUsuario = new Usuario({clave: hashed, correo} );
   console.log(nuevoUsuario);
   const isOk = await nuevoUsuario.save();
 
